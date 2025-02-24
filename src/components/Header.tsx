0570fa5c1c33e3d97f2c29546828c3afcea30598
Andrii Panaseiko <okyesanap@gmail.com>
2025-02-24 12:15:38 +0200

    Fix header css

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -11,7 +11,7 @@ const Header: React.FC = () => {
 	const {chatId} = useParams();
 
 	const links = routeHeader.filter(routeParam => headerLinks.has(routeParam.route));
-	return (<header className="fixed top-0 left-0 w-full z-10 flex flex-row justify-center">
+	return (<header className="fixed top-0 left-1/2 z-10 flex flex-row justify-center -translate-x-1/2">
 		<div className="shadow bg-gray-800 gap-4 border border-white rounded-10 p-2 m-2">
 		{links.map(routeParam => {
 			const active = chatId === routeParam.route;
